4fe775ceab73e33b95e305dea2452cef6cf2a7fd
Zachariah Dellimore <42615901+Party14534@users.noreply.github.com>
2025-01-23 14:32:05 -0500

    Made it more like the Eliza paper

--- eliza.py
+++ eliza.py
@@ -23,73 +23,49 @@ ADJECTIVE = 4
 May not need this
 '''
 
-Substitutions = [
-        r"Tell me about your \1",
-        r"Why do you \1 \2?",
-        r"How long have you been \1?",
-        r"Tell me about your \2",
-        r"Tell me about \1",
-        ]
 
-
-class Keyword:
-    word = ""
+class Substitution:
+    pattern = ""
     rank = 0
-    wordType = 0
-    substitution = 0
-    response = ""
+    output = ""
 
-    def __init__(self, word, rank, wordType, response, substitution):
-        self.word = word
+    def __init__(self, output, pattern, rank):
+        self.output = output
+        self.pattern = pattern
         self.rank = rank
-        self.wordType = wordType
-        self.substitution = substitution
-        if response:
-            self.response = response
 
-    def respond(self, input):
-        if self.response:
-            return self.response
 
-        pattern = ""
-
-        match self.substitution:
-            case 1 | 3:
-                pattern = r'.*\b(' + self.word + r')s?\b\s+\b([A-z]+)\b.*'
-            case _:
-                pattern = r'.*\b(' + self.word + r')s?\b.*'
-
-        response = re.sub(pattern, Substitutions[self.substitution], input,
-                          flags=re.IGNORECASE)
-
-        print(pattern, response)
-
-        return response
-
+Substitutions = [
+        Substitution("", r"Tell me about your \1", 5),
+        Substitution("", r"Why do you \1 \2?", 5),
+        Substitution("", r"How long have you been \1?", 5),
+        Substitution("", r"Tell me about your \2", 5),
+        Substitution("", r"Tell me about \1", 5),
+        ]
 
 # Keywords
 TopKeywords = {
-        "friend": Keyword("friend", 1, 1, None, 0),
-        "mother": Keyword("mother", 1, 1, None, 0),
-        "father": Keyword("father", 1, 1, None, 0),
-        "family": Keyword("family", 1, 1, None, 0),
-        "feel": Keyword("feel", 1, 3, None, 1),
-        "sad": Keyword("sad", 1, 4, None, 1),
-        "unhappy": Keyword("unhappy", 1, 4, None, 2),
-        "depressed": Keyword("depressed", 1, 4, None, 2),
-        "my": Keyword("my", 1, 1, None, 3)
+        "friend": [0],
+        "mother": [0],
+        "father": [0],
+        "family": [0],
+        "feel": [1],
+        "sad": [2],
+        "unhappy": [2],
+        "depressed": [2],
+        "my": [3]
         }
 
 MiddleKeywords = {
-        "because": Keyword("because", 2, 0, "Is that the real reason?", 0),
-        "alway": Keyword("always", 2, 0, "Can you give me an example of a time that happened?", 0),
-        "think": Keyword("think", 2, 0, "What caused you to think that?", 0),
-        "sometime": Keyword("sometimes", 2, 0, "Can you give me an example of a time that happened?", 0)
+        "because": Substitution("Is that the real reason?", "",  3),
+        "always": Substitution("Can you give me an example of a time that happened?", "", 3),
+        "think": Substitution("What caused you to think that?", "", 3),
+        "sometimes": Substitution("Can you give me an example of a time that happened?", "", 3)
         }
 
 BottomKeywords = {
-        "it": "What does it refer to?",
-        "maybe": "Why are you not certain?"
+        "it": Substitution("What does it refer to?", "", 1),
+        "maybe": Substitution("Why are you not certain?", "", 1)
         }
 
 ExcludeNouns = {
@@ -110,34 +86,49 @@ Fallback = [
         ]
 
 
+def getSubstitution(word, patternIndex, input):
+    wordPattern = ''
+
+    match patternIndex:
+        case 1 | 3:
+            wordPattern = r'.*\b(' + word + r')\b\s+\b([A-z]+)\b.*'
+        case _:
+            wordPattern = r'.*\b(' + word + r')\b.*'
+
+    output = re.sub(wordPattern, Substitutions[patternIndex].pattern, input,
+                    flags=re.IGNORECASE)
+
+    if output != input:
+        sub = copy.deepcopy(Substitutions[patternIndex])
+        sub.output = output
+        return sub
+
+    return None
+
+
 def ParseInput(line):
     output = []
     words = re.findall(r'\b\w+\b', line)
 
     for word in words:
-        # Substitute plurals
-        singularWord = ""
-        singularWord1 = re.sub(r'(?is)(\B)s\b', r'\1', word)
-        singularWord2 = re.sub(r'(?is)(\B)ies\b', r'\1y', word)
+        if word.lower() in TopKeywords:
+            substitutions = TopKeywords[word.lower()]
+            for substitutionIndex in substitutions:
+                substitution = getSubstitution(word, substitutionIndex, line)
+                if substitution:
+                    output.append(substitution)
 
-        if singularWord2 != word:
-            singularWord = singularWord2
-        elif singularWord1 != word:
-            singularWord = singularWord1
-        else:
-            singularWord = word
+        elif word.lower() in MiddleKeywords:
+            output.append(MiddleKeywords[word.lower()])
 
-        singularWord = singularWord.lower()
+        elif word.lower() in BottomKeywords:
+            output.append(BottomKeywords[word.lower()])
 
-        if singularWord in TopKeywords:
-            keyword = copy.deepcopy(TopKeywords[singularWord])
-            output.append(keyword)
-        elif singularWord in MiddleKeywords:
-            output.append(MiddleKeywords[singularWord])
         elif len(word) > 0 and word[0].isupper() and word.lower() not in ExcludeNouns:
             # If it is a proper noun rank it
-            output.append(Keyword(word, 1, 2, None, 4))
-            continue
+            substitution = getSubstitution(word, 4, line)
+            if substitution:
+                output.append(substitution)
 
     return output
 
@@ -155,14 +146,13 @@ def main():
         # Then parse user input and generate a response
         output = ParseInput(line)
 
-        sorted_output = sorted(output, key=lambda keyword: keyword.rank)
+        sorted_output = sorted(output, reverse=True,
+                               key=lambda substitution: substitution.rank)
 
         response = ""
 
         if len(sorted_output) > 0:
-            keyword = sorted_output[0]
-
-            response = keyword.respond(line)
+            response = sorted_output[0].output
 
         else:
             response = random.choice(Fallback)
