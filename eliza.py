9ee32b1b01225c5f126b1fb177f4a0d189156850
Zachariah Dellimore <42615901+Party14534@users.noreply.github.com>
2025-01-29 16:41:27 -0500

    Minor tweaks

--- eliza.py
+++ eliza.py
@@ -15,10 +15,11 @@ Process:
 '''
 
 
+# This class defines the substitutions
 class Substitution:
-    pattern = ""
-    rank = 0
-    output = ""
+    output = ""  # Optional output for no substitution keywords
+    pattern = ""  # Pattern to be used in the substitution
+    rank = 0  # Rank used to sort outputs
 
     def __init__(self, output, pattern, rank):
         self.output = output
@@ -26,21 +27,27 @@ class Substitution:
         self.rank = rank
 
 
+# List of pre-defined substitutions
 Substitutions = [
         Substitution("", r"Tell me about your \1", 5),
-        Substitution("", r"Why do you \1 \2?", 5),
-        Substitution("", r"How long have you been \1?", 5),
-        Substitution("", r"Tell me about your \2", 5),
+        Substitution("", r"Why do you \1 \2", 5),
+        Substitution("", r"How long have you been \1", 5),
+        Substitution("", r"Tell me about your \2", 4),
         Substitution("", r"Tell me about \1", 5),
-        Substitution("", r"When was the last time you felt \3?", 6),
-        Substitution("", r"Why do you \2 your \4?", 6),
+        Substitution("", r"When was the last time you felt \3", 6),
+        Substitution("", r"Why do you \2 your", 6),
+        Substitution("", r"Why do you feel you are", 4)
         ]
 
-# Keywords
+# Keywords link from the keyword to their substitution
 TopKeywords = {
         "friend": [0],
+        "girlfriend": [0],
+        "boyfriend": [0],
         "mother": [0],
+        "mom": [0],
         "father": [0],
+        "dad": [0],
         "sister": [0],
         "brother": [0],
         "cousin": [0],
@@ -51,20 +58,19 @@ TopKeywords = {
         "sad": [2],
         "unhappy": [2],
         "depressed": [2],
-        "my": [3, 6]
+        "my": [3, 6],
+        "am": [7]
         }
 
-MiddleKeywords = {
+NoSubKeywords = {
         "because": Substitution("Is that the real reason?", "",  3),
         "always": Substitution("Can you give me an example of a time that happened?", "", 3),
         "think": Substitution("What caused you to think that?", "", 3),
         "sometimes": Substitution("Can you give me an example of a time that happened?", "", 3),
         "today": Substitution("Can you tell me about your day?", "", 3),
-        }
-
-BottomKeywords = {
         "it": Substitution("What does it refer to?", "", 1),
-        "maybe": Substitution("Why are you not certain?", "", 1)
+        "maybe": Substitution("Why are you not certain?", "", 1),
+        "hello": Substitution("Hello, what is it you'd like to talk about?", "", 1)
         }
 
 ExcludeNouns = {
@@ -91,8 +97,10 @@ def getSubstitution(word, patternIndex, input):
     wordPattern = ''
 
     match patternIndex:
+        case 7:
+            wordPattern = r'.*\b(I)\b\s+\b(am)\b'
         case 6:
-            wordPattern = r'.*\b(I)\b\s+\b([A-z]+)\b\s+\b(my)\b\s+\b([A-z]+)\b.*'
+            wordPattern = r'.*\b(I)\b\s+\b([A-z]+)\b\s+\b(my)\b'
         case 5:
             wordPattern = r'.*\b(always)\b\s+\b(feel)\b\s+\b([A-z]+)\b.*'
         case 1 | 3:
@@ -123,11 +131,8 @@ def ParseInput(line):
                 if substitution:
                     output.append(substitution)
 
-        elif word.lower() in MiddleKeywords:
-            output.append(MiddleKeywords[word.lower()])
-
-        elif word.lower() in BottomKeywords:
-            output.append(BottomKeywords[word.lower()])
+        elif word.lower() in NoSubKeywords:
+            output.append(NoSubKeywords[word.lower()])
 
         elif len(word) > 0 and word[0].isupper() and word.lower() not in ExcludeNouns:
             # If it is a proper noun rank it
@@ -166,10 +171,12 @@ def main():
                 response = sorted_output[0].output
                 previousSubIndex = sorted_output[0].pattern
 
+            response += "?"
+
         else:
             response = random.choice(Fallback)
 
-        print(response)
+        print("ELIZA> " + response)
 
 
 if __name__ == "__main__":
